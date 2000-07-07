5e16a676fefd96e7dffb003bd0f0f234d21fbbd3
Bruce Guenter <bruce@untroubled.org>
2000-07-07 17:13:02 +0000

    Turned multiply-defined true/false constants into macros.

--- ucspi-proxy.h
+++ ucspi-proxy.h
@@ -9,4 +9,8 @@ extern void filter_server_data(char** data, ssize_t* size);
 extern void filter_deinit(void);
 extern void usage(const char*);
 
+typedef int bool;
+#define true ((bool)(0==0))
+#define false ((bool)0)
+
 #endif
