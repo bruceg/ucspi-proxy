e537873bf6e85e3b7d100ea0de9ca029e4a25976
Bruce Guenter <bruce@untroubled.org>
2000-09-19 16:00:05 +0000

    Added code to re-run relay-ctrl-allow every 5 minutes.

--- relay-filter.c
+++ relay-filter.c
@@ -1,21 +1,16 @@
+#include <signal.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include "ucspi-proxy.h"
 
-void accept_ip(const char* client_ip)
-{
-  pid_t pid;
-  
-  /* Turn off all further filtering, as this IP has already authenticated */
-  del_filter(CLIENT_IN);
-  del_filter(SERVER_IN);
-  add_filter(CLIENT_IN, write_server, 0);
-  add_filter(SERVER_IN, write_client, 0);
+extern const char* client_ip;
 
+static void run_relay_ctrl(void)
+{
   /* Run relay-ctrl-allow to allow the client's IP to relay */
-  pid = fork();
+  pid_t pid = fork();
   switch(pid) {
   case -1:
     return;
@@ -27,4 +22,23 @@ void accept_ip(const char* client_ip)
   default:
     waitpid(pid, 0, 0);
   }
+}
+
+static void catch_alarm(int ignored)
+{
+  /* Run the relay-ctrl process, and then set it up to re-run in 5 minutes */
+  run_relay_ctrl();
+  signal(SIGALRM, catch_alarm);
+  alarm(5*60);
+}
+
+void accept_client(void)
+{
+  /* Turn off all further filtering, as this IP has already authenticated */
+  del_filter(CLIENT_IN);
+  del_filter(SERVER_IN);
+  add_filter(CLIENT_IN, write_server, 0);
+  add_filter(SERVER_IN, write_client, 0);
+
+  catch_alarm(0);
 }
