c340a07a88df7bc7395349f79b9bb93e1adebb11
Bruce Guenter <bruce@untroubled.org>
2004-02-26 23:06:26 +0000

    Added manual targets to insthier.

--- insthier.c
+++ insthier.c
@@ -1,12 +1,18 @@
 #include <installer.h>
 #include "conf_bin.c"
+#include "conf_man.c"
 
 void insthier(void)
 {
-	const int bin = opendir(conf_bin);
-	c(bin, "ucspi-proxy-http-xlate", -1, -1, 0755);
-	c(bin, "ucspi-proxy-imap-relay", -1, -1, 0755);
-	c(bin, "ucspi-proxy-log",        -1, -1, 0755);
-	c(bin, "ucspi-proxy-pop3-relay", -1, -1, 0755);
-	c(bin, "ucspi-proxy",            -1, -1, 0755);
+  int bin, man, man1;
+  bin = opendir(conf_bin);
+  c(bin, "ucspi-proxy-http-xlate",    -1, -1, 0755);
+  c(bin, "ucspi-proxy-imap",          -1, -1, 0755);
+  c(bin, "ucspi-proxy-log",           -1, -1, 0755);
+  c(bin, "ucspi-proxy-pop3",          -1, -1, 0755);
+  c(bin, "ucspi-proxy",               -1, -1, 0755);
+  man = opendir(conf_man);
+  man1 = d(man, "man1", -1, -1, 0755);
+  c(man1, "ucspi-proxy-http-xlate.1", -1, -1, 0644);
+  c(man1, "ucspi-proxy.1",            -1, -1, 0644);
 }
