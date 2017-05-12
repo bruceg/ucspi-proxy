72eb6abf9d85ae8059f89720accfe9cc2bb4883c
Bruce Guenter <bruce@untroubled.org>
2017-05-12 17:43:21 -0600

    pop3-filter: Convert to using line filter mode

--- pop3-filter.c
+++ pop3-filter.c
@@ -14,23 +14,21 @@ const char filter_connfail_suffix[] = "\r\n";
 extern const char* local_name;
 
 static bool saw_command = 0;
-static str linebuf;
 
-static void handle_user(void)
+static void handle_user(str* line)
 {
   const char* ptr;
   const char* end;
 
   saw_command = 0;
-  ptr = linebuf.s + 5;
+  ptr = line->s + 5;
   while (*ptr == ' ')
     ++ptr;
-  end = linebuf.s + linebuf.len - 1;
+  end = line->s + line->len - 1;
   while (isspace(*end)) --end;
   make_username(ptr, end - ptr + 1, "USER ");
-  str_copys(&linebuf, "USER ");
-  str_cat(&linebuf, &username);
-  str_catb(&linebuf, CRLF, 2);
+  str_copys(line, "USER ");
+  str_cat(line, &username);
 }
 
 static void handle_pass(void)
@@ -38,50 +36,37 @@ static void handle_pass(void)
   saw_command = 1;
 }
 
-static void filter_client_line(void)
+static void filter_client_line(str* line)
 {
-  if (!handle_auth_response(&linebuf, 0)) {
-    if (str_case_starts(&linebuf, "PASS "))
+  if (!handle_auth_response(line, 0)) {
+    if (str_case_starts(line, "PASS "))
       handle_pass();
-    else if (str_case_starts(&linebuf, "AUTH "))
-      saw_command = handle_auth_parameter(&linebuf, 5);
-    else if (str_case_starts(&linebuf, "USER "))
-      handle_user();
+    else if (str_case_starts(line, "AUTH "))
+      saw_command = handle_auth_parameter(line, 5);
+    else if (str_case_starts(line, "USER "))
+      handle_user(line);
   }
+  write_line(line->s, line->len, write_server);
 }
 
-static void filter_client_data(char* data, ssize_t size)
-{
-  char* lf;
-  while ((lf = memchr(data, LF, size)) != 0) {
-    str_catb(&linebuf, data, lf - data + 1);
-    filter_client_line();
-    write_server(linebuf.s, linebuf.len);
-    linebuf.len = 0;
-    size -= lf - data + 1;
-    data = lf + 1;
-  }
-  str_catb(&linebuf, data, size);
-}
-
-static void filter_server_data(char* data, ssize_t size)
+static void filter_server_line(str* line)
 {
   if (saw_command) {
-    log_line(data, size);
-    if (!strncasecmp(data, "+OK ", 4)) {
+    log_line(line->s, line->len);
+    if (!strncasecmp(line->s, "+OK ", 4)) {
       accept_client(username.s);
       saw_command = 0;
     }
-    else if (!strncasecmp(data, "-ERR ", 5)) {
+    else if (!strncasecmp(line->s, "-ERR ", 5)) {
       deny_client(username.s);
       saw_command = 0;
     }
   }
-  write_client(data, size);
+  write_line(line->s, line->len, write_client);
 }
 
 void pop3_filter_init(void)
 {
-  set_filter(CLIENT_IN, filter_client_data, 0);
-  set_filter(SERVER_FD, filter_server_data, 0);
+  set_line_filter(CLIENT_IN, filter_client_line);
+  set_line_filter(SERVER_FD, filter_server_line);
 }
